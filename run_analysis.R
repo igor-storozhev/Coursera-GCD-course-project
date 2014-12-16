77890096f2080e3665f23fbdd76760a804a6411b
Igor Storozhev <igor.storozhev@gmail.com>
2014-12-16 22:23:54 +0300

    Read file corrections

--- run_analysis.R
+++ run_analysis.R
@@ -20,13 +20,18 @@
 library(dplyr)
 
 # read data files
-traning_data_file  <- read.csv(file = "train/X_train.txt")
-traning_label_file <- read.csv(file = "train/y_train.txt")
-testing_data_file  <- read.csv(file = "test/X_test.txt")
-testing_label_file <- read.csv(file = "test/y_test.txt")
+traning_data_file  <- read.csv(file = "UCI HAR Dataset/train/X_train.txt", 
+			       header = F, sep = " ")
+traning_label_file <- read.csv(file = "UCI HAR Dataset/train/y_train.txt", 
+			       header = F, sep = " ")
+testing_data_file  <- read.csv(file = "UCI HAR Dataset/test/X_test.txt", 
+			       header = F, sep = " ")
+testing_label_file <- read.csv(file = "UCI HAR Dataset/test/y_test.txt", 
+			       header = F, sep = " ")
 
 # read dictionary
-acivity_labels_file <- read.csv(file = "activity_labels.txt")
+acivity_labels_file <- read.csv(file = "UCI HAR Dataset/activity_labels.txt", 
+				header = F, sep = " ")
 
 # convert to dplyr structure
 train <- tbl_df(traning_data_file)
@@ -34,6 +39,7 @@ train_label <- tbl_df(traning_label_file)
 test  <- tbl_df(testing_data_file)
 test_label <-tbl_df(testing_label_file)
 act_label <- tbl_df(activity_labels_file)
+rm(training_data_file)
 
 # 1. merge train an test
 merged_data <- rbind_list(train, test)
